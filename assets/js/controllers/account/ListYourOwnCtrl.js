16ab379a11d9a23cf2349e20cb3dab2889a21036
Ashley <aaardentashley@gmail.com>
2015-07-06 21:06:20 -0500

    making changes to ListYourOwn view- broken now

--- assets/js/controllers/account/ListYourOwnCtrl.js
+++ assets/js/controllers/account/ListYourOwnCtrl.js
@@ -21,46 +21,37 @@ $scope.data = {};
    $scope.data.cb3 = false;
    $scope.data.cb4 = false;
 
-  
+}]);
 
   //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
   // DOM Events
   //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 
-  $scope.intent = angular.extend($scope.intent||{}, {
-// choose up to two additives checkboxes:
-// 	angular.module('checkboxDemo1', ['ngMaterial'])
-// 	.controller('ListYourOwnCtrl', function($scope) {
-// 	  $scope.data = {};
-// 	  $scope.data.cb1 = true;
-// 	  $scope.data.cb2 = false;
-// 	  $scope.data.cb3 = false;
-// 	  $scope.data.cb4 = false;
-// 	  $scope.data.cb5 = false;
-// 	});
-// // another way of providing options for "choose up to 2 additives":
-// // angular
-// //     .module('selectDemoOptGroups', ['ngMaterial'])
-// //     .controller('ListYourOwnCtrl', function($scope) {
-// //       $scope.sizes = [
-// //           "small (12-inch)",
-// //           "medium (14-inch)",
-// //           "large (16-inch)",
-// //           "insane (42-inch)"
-// //       ];
-// //       $scope.toppings = [
-// //         { category: 'meat', name: 'Pepperoni' },
-// //         { category: 'meat', name: 'Sausage' },
-// //         { category: 'meat', name: 'Ground Beef' },
-// //         { category: 'meat', name: 'Bacon' },
-// //         { category: 'veg', name: 'Mushrooms' },
-// //         { category: 'veg', name: 'Onion' },
-// //         { category: 'veg', name: 'Green Pepper' },
-// //         { category: 'veg', name: 'Green Olives' }
-// //       ];
-// //     });  
+//   $scope.intent = angular.extend($scope.intent||{}, {
 
-// // Choose a Color dropdown:
+// // // another way of providing options for "choose up to 2 additives":
+// // // angular
+// // //     .module('selectDemoOptGroups', ['ngMaterial'])
+// // //     .controller('ListYourOwnCtrl', function($scope) {
+// // //       $scope.sizes = [
+// // //           "small (12-inch)",
+// // //           "medium (14-inch)",
+// // //           "large (16-inch)",
+// // //           "insane (42-inch)"
+// // //       ];
+// // //       $scope.toppings = [
+// // //         { category: 'meat', name: 'Pepperoni' },
+// // //         { category: 'meat', name: 'Sausage' },
+// // //         { category: 'meat', name: 'Ground Beef' },
+// // //         { category: 'meat', name: 'Bacon' },
+// // //         { category: 'veg', name: 'Mushrooms' },
+// // //         { category: 'veg', name: 'Onion' },
+// // //         { category: 'veg', name: 'Green Pepper' },
+// // //         { category: 'veg', name: 'Green Olives' }
+// // //       ];
+// // //     });  
+
+// // // Choose a Color dropdown:
 //   angular.module('menuDemoBasic', ['ngMaterial'])
 //   .config(function($mdIconProvider) {
 //     $mdIconProvider
@@ -69,19 +60,8 @@ $scope.data = {};
 //   })
 //   .controller('BasicDemoCtrl', DemoCtrl);
 //   function DemoCtrl($mdDialog) {
-//     var vm = this;
-//     vm.notificationsEnabled = true;
-//     vm.toggleNotifications = function() {
-//       vm.notificationsEnabled = !vm.notificationsEnabled;
-//     };
-//     vm.redial = function(e) {
-//       $mdDialog.show(
-//         $mdDialog.alert()
-//           .title('Suddenly, a redial')
-//           .content('You just called someone back. They told you the most amazing story that has ever been told. Have a cookie.')
-//           .ok('That was easy')
-//       );
-//     };
+
+//       );};
 //     vm.checkVoicemail = function() {
 //       // This never happens.
 //     };
@@ -131,10 +111,4 @@ $scope.data = {};
 //   });
 
 
-// };
-
-
-  });
-
-console.log('im in');
-}]);
+// });
